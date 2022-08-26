3a1bf7fba48d37f5a343258d942617f094b3e918
Vadim Kaptsov <vadimkaptsov@mail.ru>
2022-08-26 17:59:07 +0300

    dark theme styles

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,8 +2,8 @@ import './main-page.sass'
 
 export default function MainPage() {
   return (
-    <div className="container">
-      <div className="page-wrapper">
+    <div className="page-wrapper">
+      <div className="container">
         <h1>Redux Toolkit</h1>
         <nav className="nav">
           <a href="/counter" className="link">
