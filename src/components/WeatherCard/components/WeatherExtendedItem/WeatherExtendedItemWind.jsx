0f2ad5d9e4f9a868b969e555f84b4bb42c7f83a2
Vadim Kaptsov <vadimkaptsov@mail.ru>
2022-10-03 15:11:04 +0300

    rm unused icon

--- src/components/WeatherCard/components/WeatherExtendedItem/WeatherExtendedItemWind.jsx
+++ src/components/WeatherCard/components/WeatherExtendedItem/WeatherExtendedItemWind.jsx
@@ -1,7 +1,5 @@
 import './weather-extended-item.scss'
 
-import '../../../../assets/icons/CompasLayoutIcon'
-
 export const WeatherExtendedItemWind = ({
   title = '',
   icon = null,
@@ -13,8 +11,6 @@ export const WeatherExtendedItemWind = ({
     <div className="weather-extended-item wind-dir-item">
       <h4 className="title">{title}</h4>
       <div className="content ">
-        {/*<CompasLayoutIcon width={50} height={50} />*/}
-
         <span
           className="icon"
           style={{ transform: `rotate(${text.value}deg)` }}
